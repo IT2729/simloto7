f35460bcf210209222fa805f8c0fa913faeebf56
IT2729 <s21c1083jp@s.chibakoudai.jp>
2024-12-29 08:05:22 +0900

    first commit

---
+++ mypkg/result.py
@@ -0,0 +1,19 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class Result(Node):
+    def __init__(self):
+        super().__init__("result")
+        self.pub = self.create_subscription(String, "loto7", self.cb, 10)
+        
+    def cb(self, msg):
+        self.get_logger().info(msg.data)
+
+
+def main():
+    rclpy.init()
+    node = Result()
+    rclpy.spin(node)
+
